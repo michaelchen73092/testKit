df439468eb120a14daa91f8aad7f8268f7700a90
zero064 <guest@guests-imac.local>
2016-06-27 18:54:50 -0700

    Initial Commit

---
+++ testKit/testKit.h
@@ -0,0 +1,19 @@
+//
+//  testKit.h
+//  testKit
+//
+//  Created by guest on 6/27/16.
+//  Copyright © 2016 guest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for testKit.
+FOUNDATION_EXPORT double testKitVersionNumber;
+
+//! Project version string for testKit.
+FOUNDATION_EXPORT const unsigned char testKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testKit/PublicHeader.h>
+
+
