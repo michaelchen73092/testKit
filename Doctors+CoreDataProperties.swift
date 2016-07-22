23c35b0765f259254999e83c961830b786fa07f5
michaelchen73092 <michaelchen73092@gmail.com>
2016-07-21 18:38:03 -0700

    update Doctor image properties

--- Doctors+CoreDataProperties.swift
+++ Doctors+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  Doctors+CoreDataProperties.swift
 //  testKit
 //
-//  Created by guest on 6/28/16.
+//  Created by CHENWEI CHIH on 7/21/16.
 //  Copyright © 2016 guest. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
@@ -14,15 +14,20 @@ import CoreData
 
 extension Doctors {
 
-    @NSManaged public var doctorHospital: String?
-    @NSManaged public var doctorOneStarNumber: NSNumber?
-    @NSManaged public var doctorFourStarNumber: NSNumber?
-    @NSManaged public var doctorThreeStarNumber: NSNumber?
-    @NSManaged public var doctorProfession: String?
-    @NSManaged public var doctorTwoStarNumber: NSNumber?
-    @NSManaged public var doctorLanguage: NSObject?
-    @NSManaged public var doctorStar: NSNumber?
     @NSManaged public var doctorFiveStarNumber: NSNumber?
+    @NSManaged public var doctorFourStarNumber: NSNumber?
     @NSManaged public var doctorGraduateSchool: String?
+    @NSManaged public var doctorHospital: String?
+    @NSManaged public var doctorLanguage: String?
+    @NSManaged public var doctorOneStarNumber: NSNumber?
+    @NSManaged public var doctorProfession: String?
+    @NSManaged public var doctorStar: NSNumber?
+    @NSManaged public var doctorThreeStarNumber: NSNumber?
+    @NSManaged public var doctorTwoStarNumber: NSNumber?
+    @NSManaged public var doctorImageMedicalLicense: NSData?
+    @NSManaged public var doctorImageDiploma: NSData?
+    @NSManaged public var doctorImageID: NSData?
+    @NSManaged public var doctorImageSpecialistLicense: NSData?
+    @NSManaged public var doctorImageCurrentHospital: NSData?
 
 }
